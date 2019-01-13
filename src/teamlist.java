1fd28534a534925a8ecb45770dd6e82b51d916ef
Hannah <hrbaez@wpi.edu>
2019-01-13 17:35:30 -0500

    hi : )

--- src/teamlist.java
+++ src/teamlist.java
@@ -2,5 +2,6 @@ public class teamlist {
     public static void main(String[] args) {
         System.out.println("Hi I'm Ben Waid");
         System.out.println("Hi I'm not Ben Waid, because I'm Parker Grant");
+        System.out.println("Hi I'm not Ben Waid, because I'm Hannah Baez");
     }
 }
