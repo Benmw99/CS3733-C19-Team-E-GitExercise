cc86bacc4045f4d0f88280e77acfdc4ece8d4180
Parker <Pagrant@wpi.edu>
2019-01-13 17:23:59 -0500

    added name

--- src/teamlist.java
+++ src/teamlist.java
@@ -1,5 +1,6 @@
 public class teamlist {
     public static void main(String[] args) {
         System.out.println("Hi I'm Ben Waid");
+        System.out.println("Hi I'm not Ben Waid, because I'm Parker Grant");
     }
 }
