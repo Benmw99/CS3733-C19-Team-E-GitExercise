4a15b320db2bdedff853de0393dce7c45bb044d2
Sierra Palmer <stpalmer@wpi.edu>
2019-01-13 17:38:06 -0500

    Added my name!

--- src/teamlist.java
+++ src/teamlist.java
@@ -3,5 +3,6 @@ public class teamlist {
         System.out.println("Hi I'm Ben Waid");
         System.out.println("Hi I'm not Ben Waid, because I'm Parker Grant");
         System.out.println("Hi I'm not Ben Waid, because I'm Hannah Baez");
+        System.out.println("Hi I'm not Hannah Baez, because I'm Sierra Palmer");
     }
 }
