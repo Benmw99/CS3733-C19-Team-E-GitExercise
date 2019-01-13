776b11e717f5874153d300e7262c803387a28693
jgold189 <climbjn189@gmail.com>
2019-01-13 17:43:48 -0500

    Added my name to it

--- src/teamlist.java
+++ src/teamlist.java
@@ -5,6 +5,5 @@ public class teamlist {
         System.out.println("Hi I'm not Ben Waid, because I'm Hannah Baez");
         System.out.println("Hi I'm not Hannah Baez, because I'm Sierra Palmer");
         System.out.println("Jordan Gold");
-        System.out.println("Again Jordan Gold")
     }
 }
